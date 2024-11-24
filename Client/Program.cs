23ef8f67a5239a25ee38378819e6c22add198a27
matvei <matveikylosov@gmail.com>
2024-11-24 21:41:01 +0500

    Перенёс класс Tools в библиотеку

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)      
+        {
+            var ip = Tools.GetInput("Укажите IP адрес сервера - ",
+                   s => IPAddress.TryParse(s, out IPAddress address) ? address : IPAddress.Any,
+                   s => true);
+
+            var port = Tools.GetInput("Укажите порт сервера - ",
+                                s => int.TryParse(s, out int p) ? p : -1,
+                                p => p > 1025 && p < 65536);
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Server.Classes;
+using Common;
 
 namespace Server
 {
